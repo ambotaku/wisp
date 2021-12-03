7b04e85794125d66e5a5ddae1cdf6ecc0ea02836
Klaus <klaus@zerbe.cloud>
2021-12-03 17:21:39 +0100

    pi pico adaption

--- wisp.cpp
+++ wisp.cpp
@@ -39,6 +39,9 @@ std::string read_file_contents(std::string filename) {
 
 #else
 #define NO_STD "no standard library support"
+#include <stdio.h>
+#include <pico/stdlib.h>
+#include <stdexcept>
 #endif
 
 
@@ -71,6 +74,7 @@ std::string read_file_contents(std::string filename) {
 #define INTERNAL_ERROR "interal virtual machine error"
 #define INDEX_OUT_OF_RANGE "index out of range"
 #define MALFORMED_PROGRAM "malformed program"
+#define OUT_OF_MEMORY "out of memory"
 
 ////////////////////////////////////////////////////////////////////////////////
 /// TYPE NAMES /////////////////////////////////////////////////////////////////
@@ -1780,6 +1784,7 @@ Value Environment::get(std::string name) const {
     throw Error(Value::atom(name), *this, ATOM_NOT_DEFINED);
 }
 
+#ifdef USE_STD
 int main(int argc, const char **argv) {
     Environment env;
     std::vector<Value> args;
@@ -1787,7 +1792,6 @@ int main(int argc, const char **argv) {
         args.push_back(Value::string(argv[i]));
     env.set("cmd-args", Value(args));
 
-    #ifdef USE_STD
     srand(time(NULL));
     try {
         if (argc == 1 || (argc == 2 && std::string(argv[1]) == "-i"))
@@ -1802,10 +1806,86 @@ int main(int argc, const char **argv) {
     } catch (std::runtime_error &e) {
         std::cerr << e.what() << std::endl;
     }
-    #else
-    if (argc == 3 && std::string(argv[1]) == "-c")
-        run(argv[2], env);
-    #endif
+}
+#else
+const uint startLineLength = 8; // the linebuffer will automatically grow for longer lines
+const char eof = 255;           // EOF in stdio.h -is -1, but getchar returns int 255 to avoid blocking
 
+/*
+ *  read a line of any  length from stdio (grows)
+ *
+ *  @param fullDuplex input will echo on entry (terminal mode) when false
+ *  @param linebreak defaults to "\n", but "\r" may be needed for terminals
+ *  @return entered line on heap - don't forget calling free() to get memory back
+ */
+char* getLine(bool fullDuplex = true, char lineBreak = '\n') {
+    // th line buffer
+    // will allocated by pico_malloc module if <cstdlib> gets included
+    char * pStart = (char*)malloc(startLineLength); 
+    char * pPos = pStart;  // next character position
+    size_t maxLen = startLineLength; // current max buffer size
+    size_t len = maxLen; // current max length
+    int c;
+
+    if(!pStart) {
+        throw std::runtime_error(OUT_OF_MEMORY); // out of memory or dysfunctional heap
+    }
+
+    while(1) {
+        c = getchar(); // expect next character entry
+        if(c == eof || c == lineBreak) {
+            break;     // non blocking exit
+        }
+
+        if (fullDuplex) {
+            putchar(c); // echo for fullDuplex terminals
+        }
+
+        if(--len == 0) { // allow larger buffer
+            len = maxLen;
+            // double the current line buffer size
+            char *pNew  = (char*)realloc(pStart, maxLen *= 2);
+            if(!pNew) {
+                free(pStart);
+                throw std::runtime_error(OUT_OF_MEMORY);
+            }
+            // fix pointer for new buffer
+            pPos = pNew + (pPos - pStart);
+            pStart = pNew;
+        }
+
+        // stop reading if lineBreak character entered 
+        if((*pPos++ = c) == lineBreak) {
+            break;
+        }
+    }
+
+    *pPos = '\0';   // set string end mark
+    return pStart;
+}
+
+int main()
+{
+    stdio_init_all(); // needed for redirect stdin/stdout to Pico's USB or UART ports
+    printf("PicoWisp Interpreter 0.1\r\n");
+    Environment env;
+    while(1) {
+        char *pLine = getLine(true, '\r');
+        if (!*pLine) {
+            continue;
+        } else {
+            try {
+                std::string input = pLine;
+                free (pLine);
+                auto tmp = run(input, env);
+                printf("\r\n%s\r\n", to_string(tmp.debug()).c_str());
+            } catch (Error &e) {
+                printf("%s\n", e.description());
+            } catch (std::runtime_error &e) {
+                printf("%s\n", e.what());
+            }
+        }
+    }
     return 0;
 }
+#endif
